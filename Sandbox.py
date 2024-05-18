1e564767853f0eb2df4b2dcd2a98ca37d315e616
AnveshJarabani <xzput@outlook.com>
2024-05-17 23:06:36 -0500

    updates

--- Sandbox.py
+++ Sandbox.py
@@ -1,36 +1,27 @@
 """
-Example 1:
+Adding two numbers that are linked list reverse orders - 
 
-Input: nums = [2,7,11,15], target = 9
-Output: [0,1]
-Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
-Example 2:
-
-Input: nums = [3,2,4], target = 6
-Output: [1,2]
-Example 3:
-
-Input: nums = [3,3], target = 6
-Output: [0,1]
-
-Solution -
-Have two pointers. Starting at either sides. Now, if the sum of the two pointers is greater than the target, move the right pointer to the left. If the sum is less than the target, move the left pointer to the right. If the sum is equal to the target, return the indices of the two pointers.
-Sorting happens in O(nlogn) time. The two pointer approach happens in O(n) time. So, the overall time complexity is O(nlogn) + O(n) = O(nlogn).
+Since the numbers are reversed in the list
+I just need to add each node values and 
+carry -> send over to the next node as value. 
+current sum // 10 -> ( to be kept in the current node value)
 """
 
 
-def twosum(nums, target):
-    nums.sort()
-    i, j = 0, len(nums) - 1
-    while i < j:
-        if nums[i] + nums[j] > target:
-            j -= 1
-        elif nums[i] + nums[j] < target:
-            i += 1
-        else:
-            return [nums[i], nums[j]]
+class ListNode:
+    def __init__(self,val=0,next=None):
+        self.val=val
+        self.next=next
 
+def addTwoNumbers(l1:ListNode,l2:ListNode) -> ListNode:
+    head = ListNode(0)
+    current = head
+    carry = 0
+    while l1 or l2 or carry:
+        sum = l1.val + l2.val
+        l1 , l2 = l1.next , l2.next
+        
+        
+        
+    
 
-nums = [2, 7, 11, 15]
-target = 9
-print(twosum(nums, target))
