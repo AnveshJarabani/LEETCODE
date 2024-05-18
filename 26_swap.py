765c776cc4a720448cb10c0e610ec369c07873cd
AnveshJarabani <xzput@outlook.com>
2024-05-18 09:20:30 -0500

    updates

--- 26_swap.py
+++ 26_swap.py
@@ -1,8 +1,28 @@
-class Solution:
-    def removeDuplicates(self, L) -> int:
-        l=1
-        for r in range(1,len(L)):
-            if L[r]!=L[r-1]:
-                L[l]=L[r]
-                l+=1
-        return l
+"""
+26 - Remove duplicates from sorted array. 
+keep a counter. 
+Input: nums = [0,0,1,1,1,2,2,3,3,4]
+Output: 5, nums = [0,1,2,3,4,_,_,_,_,_]
+two pointers left and right. 
+Keep moving the right pointer until the nums[left] != nums[right]
+then just swap values. 
+Then go left +=1 and right+=1
+do the same thing until the right value hits the last element in the array.
+"""
+
+
+def swap_duplicates(nums: list[int]) -> list[int]:
+    if not nums:
+        return 0
+    left, right = 0, 1
+    span = len(nums)
+    while right < len(nums):
+        if nums[left] != nums[right]:
+            left += 1
+            nums[left] = nums[right]
+        right += 1
+    return nums
+
+
+nums = [0, 0, 1, 1, 1, 2, 2, 3, 3, 4]
+print(swap_duplicates(nums))
