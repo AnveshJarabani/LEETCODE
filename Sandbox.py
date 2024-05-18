7d09ab0cbcaf0dbfcf21dbff3a09d46c23a5c4ce
AnveshJarabani <xzput@outlook.com>
2024-05-17 23:06:36 -0500

    updates

--- Sandbox.py
+++ Sandbox.py
@@ -1,4 +1,4 @@
-'''
+"""
 Example 1:
 
 Input: nums = [2,7,11,15], target = 9
@@ -16,21 +16,21 @@ Output: [0,1]
 Solution -
 Have two pointers. Starting at either sides. Now, if the sum of the two pointers is greater than the target, move the right pointer to the left. If the sum is less than the target, move the left pointer to the right. If the sum is equal to the target, return the indices of the two pointers.
 Sorting happens in O(nlogn) time. The two pointer approach happens in O(n) time. So, the overall time complexity is O(nlogn) + O(n) = O(nlogn).
-'''
+"""
 
-def twosum(nums,target):
+
+def twosum(nums, target):
     nums.sort()
-    i,j = 0, len(nums)-1
-    if nums[i]+nums[j] > target:
-        j-=1
-    elif nums[i]+nums[j] < target:
-        i+=1
-    else:
-        return [nums[i], nums[j]]
+    i, j = 0, len(nums) - 1
+    while i < j:
+        if nums[i] + nums[j] > target:
+            j -= 1
+        elif nums[i] + nums[j] < target:
+            i += 1
+        else:
+            return [nums[i], nums[j]]
 
 
-nums = [2,7,11,15]
+nums = [2, 7, 11, 15]
 target = 9
-print(twosum(nums,target))
-
-
+print(twosum(nums, target))
