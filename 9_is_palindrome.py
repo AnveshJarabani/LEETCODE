abb92604a1c3b7016b873347c2af676a955c3a04
AnveshJarabani <xzput@outlook.com>
2024-05-17 23:06:37 -0500

    updates

--- 9_is_palindrome.py
+++ 9_is_palindrome.py
@@ -1,15 +1,19 @@
-'''
+"""
 check if a number is palnidromic - 
 If it has a negative -> return False
 Else ->
 str(input)[:length//2]==str(input)[l//2 + 1 ::-1]
-'''
+"""
 
-def is_palindrome(i : int) -> bool:
-    if i < 0 : return False
-    print(str(i)[: len(str(i)) // 2] , str(i)[len(str(i)) // 2 + 1 :][::-1])
+
+def is_palindrome(i: int) -> bool:
+    if i < 0:
+        return False
+    print(str(i)[: len(str(i)) // 2], str(i)[len(str(i)) // 2 + 1 :][::-1])
     if len(str(i)) % 2 != 0:
         return str(i)[: len(str(i)) // 2] == str(i)[len(str(i)) // 2 + 1 :][::-1]
     return str(i)[: len(str(i)) // 2] == str(i)[len(str(i)) // 2 :][::-1]
+
+
 i = 121
 print(is_palindrome(i))
