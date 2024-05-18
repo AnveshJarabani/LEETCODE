a299fef1743c6c88d011c37c4d85df60e7d4b0e6
AnveshJarabani <xzput@outlook.com>
2024-05-17 23:06:37 -0500

    updates

--- 2_addtwonums.py
+++ 2_addtwonums.py
@@ -1,28 +1,58 @@
+"""
+Adding two numbers that are linked list reverse orders - 
+
+Since the numbers are reversed in the list
+I just need to add each node values and 
+carry -> send over to the next node as value. 
+current sum // 10 -> ( to be kept in the current node value)
+"""
+
+
 class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
         self.next = next
-class Solution:
-    def addTwoNumbers(self, l1, l2):
-        s_1,s_2=self.get_str(l1),self.get_str(l2)
-        res=str(int(s_1)+int(s_2))[::-1]
-        return build_listnode([i for i in res])
-    def get_str(self,l):
-        if not l:
-            return ""
-        return self.get_str(l.next)+str(l.val)
-import ast
-l1=ast.literal_eval(input())
-l2=ast.literal_eval(input())
-def build_listnode(l1):
-    root=ListNode(l1[0])
-    cur=root
-    for i in l1[1:]:
-        node=ListNode(i)
-        cur.next=node
-        cur=node
-    return root
-r1=build_listnode(l1)
-r2=build_listnode(l2)
 
-Solution().addTwoNumbers(r1,r2)
+    def read_list(self):
+        nodes = []
+        node = self
+        while node:
+            nodes.append(str(node.val))
+            node = node.next
+        return "->".join(nodes)
+
+
+def addTwoNumbers(l1: ListNode, l2: ListNode) -> ListNode:
+    head = ListNode(0)
+    current = head
+    carry = 0
+    while l1 or l2 or carry:
+        sum = 0
+        if l1:
+            sum += l1.val
+            l1 = l1.next
+        if l2:
+            sum += l2.val
+            l2 = l2.next
+        sum += carry
+        carry = sum // 10
+        current.next = ListNode(sum % 10)
+        current = current.next
+    return head.next
+
+
+def build_listnode(l: list) -> ListNode:
+    head = ListNode()
+    current = head
+    for i in l:
+        current.next = ListNode(val=i)
+        current = current.next
+    return head.next
+
+
+l1 = build_listnode([1, 2, 3, 4])
+l2 = build_listnode([5, 6, 7, 8, 9])
+l3 = addTwoNumbers(l1, l2)
+print(ListNode.read_list(l1))
+print(ListNode.read_list(l2))
+print(ListNode.read_list(l3))
--- Sandbox.py
+++ Sandbox.py
@@ -12,16 +12,55 @@ class ListNode:
     def __init__(self,val=0,next=None):
         self.val=val
         self.next=next
+    
+    def read_list(self):
+        nodes=[]
+        node=self
+        while node:
+            nodes.append(str(node.val))
+            node=node.next
+        return '->'.join(nodes)
+        
+        
 
 def addTwoNumbers(l1:ListNode,l2:ListNode) -> ListNode:
     head = ListNode(0)
     current = head
     carry = 0
     while l1 or l2 or carry:
-        sum = l1.val + l2.val
-        l1 , l2 = l1.next , l2.next
+        sum = 0
+        if l1:
+            sum+= l1.val
+            l1= l1.next
+        if l2:
+            sum+=l2.val
+            l2=l2.next
+        sum += carry
+        carry = sum // 10
+        current.next = ListNode(sum % 10)
+        current = current.next
+    return head.next
+        
+def build_listnode(l: list) -> ListNode:
+    head = ListNode()
+    current = head
+    for i in l:
+        current.next = ListNode(val=i)
+        current = current.next
+    return head.next
+
+l1 = build_listnode([1,2,3,4])
+l2 = build_listnode([5,6,7,8,9])
+l3 = addTwoNumbers(l1,l2)
+print(ListNode.read_list(l1))
+print(ListNode.read_list(l2))
+print(ListNode.read_list(l3))
+
+
         
         
-        
+
     
+    
+
 
