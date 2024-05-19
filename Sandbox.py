92ef3255c7c631bf1aa7860e1342b5a497447fd9
AnveshJarabani <xzput@outlook.com>
2024-05-18 20:49:04 -0500

    updates

--- Sandbox.py
+++ Sandbox.py
@@ -1,33 +1,89 @@
 """
-Given a sorted array of distinct integers and a target value,
-return the index if the target is found. If not, return the index where it would be if it were inserted in order.
-You must write an algorithm with O(log n) runtime complexity.
+Determine if a 9 x 9 Sudoku board is valid. Only the filled cells need to be validated according to the following rules:
+Each row must contain the digits 1-9 without repetition.
+Each column must contain the digits 1-9 without repetition.
+Each of the nine 3 x 3 sub-boxes of the grid must contain the digits 1-9 without repetition.
+Note:
+A Sudoku board (partially filled) could be valid but is not necessarily solvable.
+Only the filled cells need to be validated according to the mentioned rules.
 Example 1:
-Input: nums = [1,3,5,6], target = 5
-Output: 2
+Input: board = 
+[["5","3",".",".","7",".",".",".","."]
+,["6",".",".","1","9","5",".",".","."]
+,[".","9","8",".",".",".",".","6","."]
+,["8",".",".",".","6",".",".",".","3"]
+,["4",".",".","8",".","3",".",".","1"]
+,["7",".",".",".","2",".",".",".","6"]
+,[".","6",".",".",".",".","2","8","."]
+,[".",".",".","4","1","9",".",".","5"]
+,[".",".",".",".","8",".",".","7","9"]]
+Output: true
 Example 2:
-Input: nums = [1,3,5,6], target = 2
-Output: 1
-Example 3:
-Input: nums = [1,3,5,6], target = 7
-Output: 4
+Input: board = 
+[["8","3",".",".","7",".",".",".","."]
+,["6",".",".","1","9","5",".",".","."]
+,[".","9","8",".",".",".",".","6","."]
+,["8",".",".",".","6",".",".",".","3"]
+,["4",".",".","8",".","3",".",".","1"]
+,["7",".",".",".","2",".",".",".","6"]
+,[".","6",".",".",".",".","2","8","."]
+,[".",".",".","4","1","9",".",".","5"]
+,[".",".",".",".","8",".",".","7","9"]]
+Output: false
+Explanation: Same as Example 1, except with the 5 in the top left corner being modified to 8.
+Since there are two 8's in the top left 3x3 sub-box, it is invalid.
 
-bst - 
-search for the value and it doesn't exist, just give the median where it should be placed.
+Approach - 
+do three dicts - 
+row, col , square to keep track of each of the rows, cols and squars and if they have issues. 
+if the value is within 0 and 9 and not repeated in row
+if the value is within 0 and 9 and not repeated in col
+if the value is within 0 and 9 and not repeated in square
 """
 
-def search_insert_position(nums : list[int], target: int) -> int:
-    left, right = 0, len(nums) -1
-    while left <= right:
-        mid = (left + right) // 2
-        if nums[mid] == target:
-            return mid
-        elif nums[mid] < target:
-            left = mid + 1
-        else:
-            right = mid - 1
-    return left
+from collections import defaultdict as dct
 
-print(search_insert_position(nums = [1,3,5,6], target = 5) == 2)
-print(search_insert_position(nums = [1,3,5,6], target = 2) == 1)
-print(search_insert_position(nums = [1,3,5,6], target = 7) == 4)
+
+def isvalid_sudoku(board: list[list[str]]) -> bool:
+    row, col, square = dct(set), dct(set), dct(set)
+    for i in range(len(board)):
+        for j in range(len(board[0])):
+            if board[i][j] == ".":
+                continue
+            val = int(board[i][j])
+            if (
+                val < 0
+                or val > 9
+                or val in row[i]
+                or val in col[j]
+                or val in square[(i, j)]
+            ):
+                return False
+            row[i].add(val)
+            col[j].add(val)
+            square[(i, j)].add(val)
+    return True
+
+
+board1 = \
+[["5","3",".",".","7",".",".",".","."]
+,["6",".",".","1","9","5",".",".","."]
+,[".","9","8",".",".",".",".","6","."]
+,["8",".",".",".","6",".",".",".","3"]
+,["4",".",".","8",".","3",".",".","1"]
+,["7",".",".",".","2",".",".",".","6"]
+,[".","6",".",".",".",".","2","8","."]
+,[".",".",".","4","1","9",".",".","5"]
+,[".",".",".",".","8",".",".","7","9"]]
+board2 = \
+[["8","3",".",".","7",".",".",".","."]
+,["6",".",".","1","9","5",".",".","."]
+,[".","9","8",".",".",".",".","6","."]
+,["8",".",".",".","6",".",".",".","3"]
+,["4",".",".","8",".","3",".",".","1"]
+,["7",".",".",".","2",".",".",".","6"]
+,[".","6",".",".",".",".","2","8","."]
+,[".",".",".","4","1","9",".",".","5"]
+,[".",".",".",".","8",".",".","7","9"]]
+print(isvalid_sudoku(board1))
+print(isvalid_sudoku(board2))
--- valid_sudoku.py
+++ valid_sudoku.py
@@ -1,50 +1,91 @@
 """
-Sudoku Solver
-Write the function sudokuSolve that checks whether a given sudoku board (i.e. sudoku puzzle) is solvable. If so, the function will returns true. Otherwise (i.e. there is no valid solution to the given sudoku board), returns false.
+Determine if a 9 x 9 Sudoku board is valid. Only the filled cells need to be validated according to the following rules:
+Each row must contain the digits 1-9 without repetition.
+Each column must contain the digits 1-9 without repetition.
+Each of the nine 3 x 3 sub-boxes of the grid must contain the digits 1-9 without repetition.
+Note:
+A Sudoku board (partially filled) could be valid but is not necessarily solvable.
+Only the filled cells need to be validated according to the mentioned rules.
+Example 1:
+Input: board = 
+[["5","3",".",".","7",".",".",".","."]
+,["6",".",".","1","9","5",".",".","."]
+,[".","9","8",".",".",".",".","6","."]
+,["8",".",".",".","6",".",".",".","3"]
+,["4",".",".","8",".","3",".",".","1"]
+,["7",".",".",".","2",".",".",".","6"]
+,[".","6",".",".",".",".","2","8","."]
+,[".",".",".","4","1","9",".",".","5"]
+,[".",".",".",".","8",".",".","7","9"]]
+Output: true
+Example 2:
+Input: board = 
+[["8","3",".",".","7",".",".",".","."]
+,["6",".",".","1","9","5",".",".","."]
+,[".","9","8",".",".",".",".","6","."]
+,["8",".",".",".","6",".",".",".","3"]
+,["4",".",".","8",".","3",".",".","1"]
+,["7",".",".",".","2",".",".",".","6"]
+,[".","6",".",".",".",".","2","8","."]
+,[".",".",".","4","1","9",".",".","5"]
+,[".",".",".",".","8",".",".","7","9"]]
+Output: false
+Explanation: Same as Example 1, except with the 5 in the top left corner being modified to 8.
+Since there are two 8's in the top left 3x3 sub-box, it is invalid.
 
-In sudoku, the objective is to fill a 9x9 board with digits so that each column, each row, and each of the nine 3x3 sub-boards that compose the board contains all of the digits from 1 to 9. The board setter provides a partially completed board, which for a well-posed board has a unique solution. As explained above, for this problem, it suffices to calculate whether a given sudoku board has a solution. No need to return the actual numbers that make up a solution.
-
-A sudoku board is represented as a two-dimensional 9x9 array of the characters ‘1’,‘2’,…,‘9’ and the '.' character, which represents a blank space. The function should fill the blank spaces with characters such that the following rules apply:
-
-In every row of the array, all characters ‘1’,‘2’,…,‘9’ appear exactly once.
-In every column of the array, all characters ‘1’,‘2’,…,‘9’ appear exactly once.
-In every 3x3 sub-board that is illustrated below, all characters ‘1’,‘2’,…,‘9’ appear exactly once.
-A solved sudoku is a board with no blank spaces, i.e. all blank spaces are filled with characters that abide to the constraints above. If the function succeeds in solving the sudoku board, it’ll return true (false, otherwise).
-
+Approach - 
+do three dicts - 
+row, col , square to keep track of each of the rows, cols and squars and if they have issues. 
+if the value is within 0 and 9 and not repeated in row
+if the value is within 0 and 9 and not repeated in col
+if the value is within 0 and 9 and not repeated in square
 """
 
-from collections import defaultdict
+from collections import defaultdict as dct
 
 
-def sudoku_solve(board):
-    col = defaultdict(set)
-    row = defaultdict(set)
-    square = defaultdict(set)
-    for i in range(9):
-        for j in range(9):
+def isvalid_sudoku(board: list[list[str]]) -> bool:
+    row, col, square = dct(set), dct(set), dct(set)
+    for i in range(len(board)):
+        for j in range(len(board[0])):
             if board[i][j] == ".":
                 continue
+            val = int(board[i][j])
             if (
-                board[i][j] in row[i]
-                or board[i][j] in col[j]
-                or board[i][j] in square[(i // 3, j // 3)]
+                val < 0
+                or val > 9
+                or val in row[i]
+                or val in col[j]
+                or val in square[(i, j)]
             ):
                 return False
-            row[i].add(board[i][j])
-            col[j].add(board[i][j])
-            square[(i // 3, j // 3)].add(board[i][j])
+            row[i].add(val)
+            col[j].add(val)
+            square[(i, j)].add(val)
     return True
 
 
-board = [
-    [".", "2", "3", "4", "5", "6", "7", "8", "9"],
-    ["1", ".", ".", ".", ".", ".", ".", ".", "."],
-    [".", ".", ".", ".", ".", ".", ".", ".", "."],
-    [".", ".", ".", ".", ".", ".", ".", ".", "."],
-    [".", ".", ".", ".", ".", ".", ".", ".", "."],
-    [".", ".", ".", ".", ".", ".", ".", ".", "."],
-    [".", ".", ".", ".", ".", ".", ".", ".", "."],
-    [".", ".", ".", ".", ".", ".", ".", ".", "."],
-    [".", ".", ".", ".", ".", ".", ".", ".", "."],
+board1 = [
+    ["5", "3", ".", ".", "7", ".", ".", ".", "."],
+    ["6", ".", ".", "1", "9", "5", ".", ".", "."],
+    [".", "9", "8", ".", ".", ".", ".", "6", "."],
+    ["8", ".", ".", ".", "6", ".", ".", ".", "3"],
+    ["4", ".", ".", "8", ".", "3", ".", ".", "1"],
+    ["7", ".", ".", ".", "2", ".", ".", ".", "6"],
+    [".", "6", ".", ".", ".", ".", "2", "8", "."],
+    [".", ".", ".", "4", "1", "9", ".", ".", "5"],
+    [".", ".", ".", ".", "8", ".", ".", "7", "9"],
 ]
-print(sudoku_solve(board))
+board2 = [
+    ["8", "3", ".", ".", "7", ".", ".", ".", "."],
+    ["6", ".", ".", "1", "9", "5", ".", ".", "."],
+    [".", "9", "8", ".", ".", ".", ".", "6", "."],
+    ["8", ".", ".", ".", "6", ".", ".", ".", "3"],
+    ["4", ".", ".", "8", ".", "3", ".", ".", "1"],
+    ["7", ".", ".", ".", "2", ".", ".", ".", "6"],
+    [".", "6", ".", ".", ".", ".", "2", "8", "."],
+    [".", ".", ".", "4", "1", "9", ".", ".", "5"],
+    [".", ".", ".", ".", "8", ".", ".", "7", "9"],
+]
+print(isvalid_sudoku(board1))
+print(isvalid_sudoku(board2))
