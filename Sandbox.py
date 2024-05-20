aa67d1364789f5cf3164707334fcdfe3d46b6433
AnveshJarabani <xzput@outlook.com>
2024-05-20 11:00:37 -0500

    updates

--- Sandbox.py
+++ Sandbox.py
@@ -1,69 +1,46 @@
 """
-Solve sudoku board - 
-1. Create a function to check if the current number in consideration
-is a valid option to specify in the sudoku board. 
-If it is , then we enter it. 
-We do this recursively for each element of "123456789"
-If at any point it's a false, We return False, else keep going till the last value. 
+Given an array of distinct integers candidates and a target integer target,
+return a list of all unique combinations of candidates where the chosen numbers sum to target. You may return the combinations in any order.
+The same number may be chosen from candidates an unlimited number of times.
+Two combinations are unique if the frequency of at least one of the chosen numbers is different.
+The test cases are generated such that the number of unique combinations that sum up to target is less than 150 combinations for the given input.
+Example 1:
+Input: candidates = [2,3,6,7], target = 7
+Output: [[2,2,3],[7]]
+Explanation:
+2 and 3 are candidates, and 2 + 2 + 3 = 7. Note that 2 can be used multiple times.
+7 is a candidate, and 7 = 7.
+These are the only two combinations.
+Example 2:
+Input: candidates = [2,3,5], target = 8
+Output: [[2,2,2,2],[2,3,3],[3,5]]
+Example 3:
+Input: candidates = [2], target = 1
+Output: []
+
+Approach - 
+1. Backtrack solution. start with [] / empty list and keep going like that.
+Big O Time - log(N^(T/M)) and space - log(T/M)
 """
 
 
-def solve_sudoku(board: list[list[str]]) -> list[list[str]]:
-    # Find if the number can be used at the current position in the board
-    def is_valid(board, row, col, num):
-        for x in range(9):
-            if board[row][x] == num:
-                return False
-            if board[x][col] == num:
-                return False
-        start_row, start_col = row - row % 3, col - col % 3
-        for x in range(3):
-            for y in range(3):
-                if board[start_row + x][start_col + y] == num:
-                    return False
-        return True
-    def solve(board):
-        for row in range(9):
-            for col in range(9):
-                if board[row][col] == '.':
-                    s = '123456789'
-                    for num in s:
-                        if is_valid(board,row,col,num):
-                            board[row][col] = num
-                            if solve(board):
-                                return True
-                        board[row][col] = '.'
-                    return False
-        return True
-    
-    if not board: return None
-    if solve(board): return board
-                    
+def combination_sum(candidates: list[int], target: int) -> list[list[int]]:
+    def backtrack(current_sum: int, combination: list, idx: int):
+        if current_sum == 0:
+            result.append(list(combination))
+            return
+        elif current_sum < 0:
+            return
+        for i in range(idx, len(candidates)):
+            combination.append(candidates[i])
+            backtrack(current_sum - candidates[i], combination, i)
+            combination.pop()
 
-        
+    result = []
+    backtrack(current_sum=target, combination=[], idx=0)
+    return result
 
 
-board = [
-    ["5", "3", ".", ".", "7", ".", ".", ".", "."],
-    ["6", ".", ".", "1", "9", "5", ".", ".", "."],
-    [".", "9", "8", ".", ".", ".", ".", "6", "."],
-    ["8", ".", ".", ".", "6", ".", ".", ".", "3"],
-    ["4", ".", ".", "8", ".", "3", ".", ".", "1"],
-    ["7", ".", ".", ".", "2", ".", ".", ".", "6"],
-    [".", "6", ".", ".", ".", ".", "2", "8", "."],
-    [".", ".", ".", "4", "1", "9", ".", ".", "5"],
-    [".", ".", ".", ".", "8", ".", ".", "7", "9"],
-]
-test_result = [
-    ["5", "3", "4", "6", "7", "8", "9", "1", "2"],
-    ["6", "7", "2", "1", "9", "5", "3", "4", "8"],
-    ["1", "9", "8", "3", "4", "2", "5", "6", "7"],
-    ["8", "5", "9", "7", "6", "1", "4", "2", "3"],
-    ["4", "2", "6", "8", "5", "3", "7", "9", "1"],
-    ["7", "1", "3", "9", "2", "4", "8", "5", "6"],
-    ["9", "6", "1", "5", "3", "7", "2", "8", "4"],
-    ["2", "8", "7", "4", "1", "9", "6", "3", "5"],
-    ["3", "4", "5", "2", "8", "6", "1", "7", "9"],
-]
-print(solve_sudoku(board))
-print(solve_sudoku(board) == test_result)
+print(combination_sum([2, 3, 6, 7], 7))
+print(combination_sum([2, 3, 5], 8))
+print(combination_sum([2], 1))
