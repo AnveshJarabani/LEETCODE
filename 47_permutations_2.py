1613d488fc0980cb8f269039496e3ffd43a3462e
AnveshJarabani <xzput@outlook.com>
2024-05-20 19:54:31 -0500

    updates

--- 47_permutations_2.py
+++ 47_permutations_2.py
@@ -1,19 +1,38 @@
-from typing import List
-class Solution:
-    def permuteUnique(self, nums: List[int]) -> List[List[int]]:
-        hash_map={n:nums.count(n) for n in nums}
-        def df():
-            if len(temp)==len(nums):
-                result.append(temp.copy())
-                return
-            for n in hash_map:
-                if hash_map[n]>0:
-                    temp.append(n)
-                    hash_map[n]-=1
-                    df()
-                    hash_map[n]+=1
-                    temp.pop()
-        result,temp=[],[]
-        df()
-        return result
-print(Solution().permuteUnique([1,1,3]))
+"""
+Given a collection of numbers, nums, that might contain duplicates, return all possible unique permutations in any order.
+Example 1:
+Input: nums = [1,1,2]
+Output: [[1,1,2],[1,2,1],[2,1,1]]
+Example 2:
+Input: nums = [1,2,3]
+Output: [[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
+"""
+
+from collections import Counter
+
+
+def unique_permutations(nums: list[int]) -> list[list[int]]:
+    hash_map = Counter(nums)
+
+    def dfs():
+        if len(temp) == len(nums):
+            res.append(temp.copy())
+            return
+        for n in hash_map:
+            if hash_map[n] > 0:
+                temp.append(n)
+                hash_map[n] -= 1
+                dfs()
+                hash_map[n] += 1
+                temp.pop()
+
+    res, temp = [], []
+    dfs()
+    return res
+
+
+print(unique_permutations(nums=[1, 1, 2]), "res = [[1,1,2],[1,2,1],[2,1,1]]")
+print(
+    unique_permutations(nums=[1, 2, 3]),
+    "res = [[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]",
+)
--- Sandbox.py
+++ Sandbox.py
@@ -1,34 +1,38 @@
 """
-Given an array nums of distinct integers, return all the possible permutations. You can return the answer in any order.
+Given a collection of numbers, nums, that might contain duplicates, return all possible unique permutations in any order.
 Example 1:
+Input: nums = [1,1,2]
+Output: [[1,1,2],[1,2,1],[2,1,1]]
+Example 2:
 Input: nums = [1,2,3]
 Output: [[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
-Example 2:
-Input: nums = [0,1]
-Output: [[0,1],[1,0]]
-Example 3:
-Input: nums = [1]
-Output: [[1]]
-
-Approach - 
-use backtrack algo to find all combinations and just return
 """
 
-
-def permute(nums: list[int]) -> list[list[int]]:
-    def backtrack(nums, path):
-        if not nums:
-            result.append(path)
-        for i in range(len(nums)):
-            backtrack(nums[:i] + nums[i + 1 :], path + [nums[i]])
-
-    result = []
-    backtrack(nums, [])
-    return result
+from collections import Counter
 
 
+def unique_permutations(nums: list[int]) -> list[list[int]]:
+    hash_map = Counter(nums)
+
+    def dfs():
+        if len(temp) == len(nums):
+            res.append(temp.copy())
+            return
+        for n in hash_map:
+            if hash_map[n] > 0:
+                temp.append(n)
+                hash_map[n] -= 1
+                dfs()
+                hash_map[n] += 1
+                temp.pop()
+
+    res, temp = [], []
+    dfs()
+    return res
+
+
+print(unique_permutations(nums=[1, 1, 2]), "res = [[1,1,2],[1,2,1],[2,1,1]]")
 print(
-    permute(nums=[1, 2, 3]), " res = [[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]"
+    unique_permutations(nums=[1, 2, 3]),
+    "res = [[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]",
 )
-print(permute(nums=[0, 1]), "[[0,1],[1,0]]")
-print(permute(nums=[1]), "[[1]]")
