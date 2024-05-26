41424321fc36579f51a1e884e60c0a45ba2633f3
AnveshJarabani <xzput@outlook.com>
2024-05-26 14:17:53 -0500

    updates

--- 56.merge_intervals.py
+++ 56.merge_intervals.py
@@ -1,12 +1,30 @@
-def merge(intervals):
-    intervals.sort()
-    result=[intervals[0]]
-    for indx in range(1,len(intervals)):
-        if result[-1][1]>=intervals[indx][0]:
-            temp=result[-1]+intervals[indx]
-            result[-1]=[min(temp),max(temp)]
+"""
+Given an array of intervals where intervals[i] = [starti, endi],
+merge all overlapping intervals, and return an array of the non-overlapping intervals that cover all the intervals in the input.
+Example 1:
+Input: intervals = [[1,3],[2,6],[8,10],[15,18]]
+Output: [[1,6],[8,10],[15,18]]
+Explanation: Since intervals [1,3] and [2,6] overlap, merge them into [1,6].
+Example 2:
+Input: intervals = [[1,4],[4,5]]
+Output: [[1,5]]
+Explanation: Intervals [1,4] and [4,5] are considered overlapping.
+"""
+
+
+def merge_intervals(intervals: list[list[int]]) -> list[list[int]]:
+    intervals.sort(key=lambda x: x[0])
+    merged = []
+    for interval in intervals:
+        if not merged or merged[-1][1] < interval[0]:
+            merged.append(interval)
         else:
-            result.append(intervals[indx])
-    return result
-intervals=[[1,4],[0,0]]
-print(merge(intervals))
+            merged[-1][1] = max(merged[-1][1], interval[1])
+    return merged
+
+
+print(
+    merge_intervals(intervals=[[1, 3], [2, 6], [8, 10], [15, 18]]),
+    "Output: [[1,6],[8,10],[15,18]]",
+)
+print(merge_intervals(intervals=[[1, 4], [4, 5]]), "Output: [[1,5]]")
