0bdfe4662e24819c06446980516a730c00d34b42
AnveshJarabani <xzput@outlook.com>
2024-05-26 17:45:31 -0500

    updates

--- 59_spiral_matrix_2.py
+++ 59_spiral_matrix_2.py
@@ -8,15 +8,17 @@ Example 2:
 Input: n = 1
 Output: [[1]]
 """
+
+
 def spiral_matrix(n: int) -> list[list[int]]:
-    result = [[0]*n for _ in range(n)]
-    top, bottom, left, right, num = 0, n-1,0, n-1, 1
+    result = [[0] * n for _ in range(n)]
+    top, bottom, left, right, num = 0, n - 1, 0, n - 1, 1
     while top <= bottom and left <= right:
-        for i in range(left , right+1):
+        for i in range(left, right + 1):
             result[top][i] = num
             num += 1
         top += 1
-        for i in range(top , bottom + 1):
+        for i in range(top, bottom + 1):
             result[i][right] = num
             num += 1
         right -= 1
@@ -29,6 +31,7 @@ def spiral_matrix(n: int) -> list[list[int]]:
             num += 1
         left += 1
     return result
-            
-print(spiral_matrix(n = 3),"Output: [[1,2,3],[8,9,4],[7,6,5]]")
-print(spiral_matrix(n = 1),"Output: [[1]]")
+
+
+print(spiral_matrix(n=3), "Output: [[1,2,3],[8,9,4],[7,6,5]]")
+print(spiral_matrix(n=1), "Output: [[1]]")
