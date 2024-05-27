2153f09a4bc4c867fab2c3105961abf4a4bfd18f
AnveshJarabani <xzput@outlook.com>
2024-05-26 19:09:25 -0500

    updates

--- 61_rotate_list.py
+++ 61_rotate_list.py
@@ -21,18 +21,21 @@ from typing import Optional
 
 class Solution:
     def rotate_list(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
+        if not head: 
+            return None
         # Calculate the length of the node list
+        length = 1
         cur_node = ListNode()
         cur_node.next = head
-        length = 1
         while cur_node.next:
             cur_node = cur_node.next
             length += 1
+        # now the cur_node is at the tail of the listnode
         k %= length
-        cur_node.next = head
-        cur_node = cur_node.next
+        cur_node.next = head #! attaching the tail to head here
         for _ in range(length - k - 1):
-            cur_node.next = cur_node
+            cur_node = cur_node.next
         result = cur_node.next
         cur_node.next = None
         return result
+        
